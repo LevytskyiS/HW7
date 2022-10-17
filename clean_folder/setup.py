d1d464be11ca9dfb09ce225e3ba2cc05b3f54ee8
LevytskyiS <levytskyi.sv@gmail.com>
2022-10-17 14:57:18 +0200

    Update setup.py
    Added function 'main' in L12.

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -9,5 +9,5 @@ setup(
     author_email='berzerksn@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['clean-folder = clean_folder.clean']}
+    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']}
     )
